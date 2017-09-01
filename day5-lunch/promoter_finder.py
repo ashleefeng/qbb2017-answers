ab98dc3013140db02d71c28f24b60bc3243ce1b6
Xinyu (Ashlee) Feng <ashleefeng7@gmail.com>
2017-09-01 12:40:03 -0400

    q2

---
+++ day5-lunch/promoter_finder.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+usage: ./promoter_finder.py <ctab file> <output filename>
+"""
+
+import sys
+import pandas as pd
+
+df = pd.read_csv(sys.argv[1], sep='\t')
+output = []
+
+for index, row in df.iterrows():
+    row_out = {}
+    row_out["chromosome"] = row["chr"]
+    row_out["t_name"] = row["t_name"]
+    if row["strand"] == "+":
+        row_out["start"] = row["start"] - 500
+        row_out["end"] = row["start"] + 500
+    elif row["strand"] == "-":
+        row_out["start"] = row["end"] - 500
+        row_out["end"] = row["end"] + 500
+    
+    output.append(row_out)
+
+df_out = pd.DataFrame(output)
+df_out.to_csv(sys.argv[2] + ".bed", sep = '\t')
