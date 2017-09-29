84d571ff36d7313b7ed375644d181564ee7bbe32
Xinyu (Ashlee) Feng <ashleefeng7@gmail.com>
2017-09-29 00:37:47 -0400

    week3

--- week3/00-af_spectrum.py
+++ week3/00-af_spectrum.py
@@ -7,11 +7,13 @@ Usage: ./00-af_spectrum.py <vcf file> <output>
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
+import numpy as np
 
 file = open(sys.argv[1])
 output = sys.argv[2]
 
 afs = []
+quals = []
 
 for line in file:
     
@@ -19,6 +21,10 @@ for line in file:
         continue
     else:
         tokens = line.rstrip('\n').split('\t')
+        
+        qual = float(tokens[5])
+        quals.append(qual)
+        
         info = tokens[7]
         info_tokens = info.split(';')
         af = info_tokens[3].split('=')[1]
@@ -32,3 +38,13 @@ plt.ylabel('Count')
 plt.title('Allele Spectrum')
 plt.savefig(output)
 plt.close()
+
+file.close()
+file = open(sys.argv[1])
+
+df = pd.read_csv(file, sep = '\t', header = 82)
+
+quals_sort_index = np.argsort(quals)
+hiqual_ids = quals_sort_index[-5:]
+hiqual_df = df.iloc[hiqual_ids]
+print hiqual_df.to_csv(sep='\t')
